bda5fa3535d3f0b49c05eee77b367fe39861357c
Adam Dockstader <39977252+AdamDock@users.noreply.github.com>
2023-12-19 14:11:40 -0700

    Completed house cards generation

--- my-app/src/components/decision.tsx
+++ my-app/src/components/decision.tsx
@@ -32,7 +32,7 @@ class Deck {
     initializeDeck(): void {
       const suits: string[] = ['H', 'D', 'C', 'S'];
       const ranks: string[] = ['A','2', '3', '4', '5', '6', '7', '8', '9', '0', 'J', 'Q', 'K'];
-      const values: number[] = [1,2,3,4,5,6,7,8,9,10,11,12,13];
+      const values: number[] = [1,2,3,4,5,6,7,8,9,10,10,10,10];
   
       suits.forEach((suit) => {
         ranks.forEach((rank) => {
@@ -43,7 +43,6 @@ class Deck {
     }
   
     shuffle(): void {
-      // Fisher-Yates shuffle algorithm
       for (let i = this.cards.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
@@ -64,12 +63,13 @@ class Deck {
       this.shuffle();
     }
   }
+  const deck = new Deck();
+    deck.shuffle();
   
  
 
 const Decision: React.FC<DecisionProps> = ({ getWinnings }) => {
-    const deck = new Deck();
-    deck.shuffle();
+    
   const [bet, setBet] = useState<number>(0);
   const [stack, setStack] = useState<number>(5000);
 const [winnings, setWinnings] = useState<number>(0);
@@ -77,16 +77,50 @@ const [dealerScore, setDealerScore] = useState<number>(0);
 const [playerScore, setPlayerScore] = useState<number>(0);
 const [dealerCards, setDealerCards] = useState<Card[]>([]);
 const [playerCards, setPlayerCards] = useState<Card[]>([]);
+const [betLock, setBetLock] = useState<boolean>(false);
+const [gameOver, setGameOver] = useState<boolean>(false);
+const [playerLock, setPlayerLock] = useState<boolean>(false);
+const [dealClicked, setDealClicked] = useState<boolean>(false);
+
+const gameScript = () => {
+}
 
 const onClickDeal = () => {
+    if(!dealClicked){
     setPlayerCards([deck.dealCard()!, deck.dealCard()!]);
     setDealerCards([deck.dealCard()!, deck.dealCard()!]);
-    setStack(stack+(bet*2));
-    console.log(playerCards);
-    console.log(dealerCards);
-    
-    
+    setBetLock(true);  
+    setDealClicked(true);
+    }
+  
 }
+useEffect(() => {
+    if(playerScore > 21){
+        setPlayerLock(true)
+    }
+},[playerScore]);
+useEffect(() => {
+    const dealerTurn = async () => {
+      if (deck.cards.length > 0 && playerLock) {
+        while (dealerScore < 17) {
+             console.log(dealerScore);
+          await new Promise((resolve) => setTimeout(resolve, 500));
+          setDealerCards((prevDealerCards) => [...prevDealerCards, deck.dealCard()!]);
+          setDealerScore((prevDealerScore) => prevDealerScore + deck.dealCard()!.value);
+        }
+      }
+    };
+  
+    const performDealerTurn = async () => {
+      if (playerLock || playerScore > 21) {
+        await dealerTurn();
+      }
+    };
+  
+    performDealerTurn();
+  }, [playerLock, playerScore, dealerScore, deck.cards, dealerCards]);
+  
+  
 useEffect(() => {
     const dealerTotal = dealerCards.reduce((acc, card) => acc + card.value, 0);
     setDealerScore(dealerTotal);
@@ -94,38 +128,63 @@ useEffect(() => {
   useEffect(() => {
     const playerTotal = playerCards.reduce((acc, card) => acc + card.value, 0);
     setPlayerScore(playerTotal);
+    console.log({...deck});
+    console.log(playerCards);
+    console.log(dealerCards);
   },[playerCards]);
+  
     
 const onClickHit = () => {
-    if (deck.cards.length > 0) {
+    if(!playerLock){if (deck.cards.length > 0) {
         setPlayerCards([...playerCards, deck.dealCard()!]);
         console.log(playerCards);
     } else {
         console.log('No more cards in the deck.');
     }
+    console.log(deck);
+}
+}
+const onClickStay = () => {
+    setPlayerLock(true);
+    
+}
+const onClickSplit = () => {
+}
+const onClickDouble = () => {
+    if(!playerLock){
+        setStack(stack-bet);
+        setBet(bet*2);
+        setPlayerCards([...playerCards, deck.dealCard()!]);
+        setPlayerLock(true);
+}
 }
 
 const handleChipClick = (amount: number): void => {
     // Check if the bet amount is within the remaining stack
-    if (amount <= stack) {
+    if(!betLock){if (amount <= stack) {
       setBet((prevBet) => prevBet + amount);
       setStack((prevStack) => prevStack - amount);
     } else {
       // Display alert if the bet amount is more than the remaining stack
       alert("More than remaining bet amount. Try again.");
     }
+}
   };
 
   const handleResetClick = (): void => {
     // Move all chips from the pot back to the stack
+    if(!betLock){
     setStack((prevStack) => prevStack + bet);
     setBet(0);
+    }
   };
 
   const handleAllInClick = (): void => {
     // Move all chips from the stack to the pot
+    if(!betLock){
     setBet((prevBet) => prevBet + stack);
     setStack(0);
+    }
   };
 
   const chip = (amount: number): JSX.Element => (
@@ -252,9 +311,9 @@ const handleChipClick = (amount: number): void => {
         <div><h2>Bet total: {bet}</h2></div>
         <div><button className={styles.button} onClick={onClickDeal}>Deal</button></div>
         <div><button className={styles.button} onClick={onClickHit}>Hit</button></div>
-        <div><button className={styles.button}>Stay</button></div>
-        <div><button className={styles.button}>Split</button></div>
-        <div><button className={styles.button}>Double</button></div>
+        <div><button className={styles.button} onClick={onClickStay}>Stay</button></div>
+        <div><button className={styles.button} onClick={onClickSplit}>Split</button></div>
+        <div><button className={styles.button} onClick={onClickDouble}>Double</button></div>
         <div><h2>Stack: {stack}</h2></div>
       </div>
       <div className={styles.row}>
