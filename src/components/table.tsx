327c02d89437d79a4b0dc9c6c2e04f5a93c2dff8
Adam Dockstader <39977252+AdamDock@users.noreply.github.com>
2024-02-03 00:13:10 -0700

    Adjusted introductory modal for styling.

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -47,28 +47,33 @@ const Table = () => {
   );
   const modal2 = (
     <Modal onClose={handleClose2} actionBar={actionBar2}>
-        <div>
-        <h1>Are you ready to play blackjack?</h1>
+        <div className="flex-row justify-center m-10">
+          <div className="text-center text-2xl font-bold">
+          <h1>Are you ready to play blackjack?</h1>
         <h1>Start with 5000 chips and try your luck in this casino game!</h1>
-        <div className="text-medium font-medium">
+
+          </div>
+        
+        <div className="text-medium font-medium ml-10 mr-20 mt-7">
         <h1 className="text-lg font-bold">Steps</h1>
         <ol>
-          <li>
+          <li className="p-3">
             1.) Place a bet by clicking on the chips. Click reset if you need to restart.
           </li>
-          <li>
+          <li className="p-3">
             2.) Click Deal to be dealt a hand and the dealer will be dealt a hand as well.
           </li>
-          <li>
-            3.) Click Hit to be dealt another card. You can hit as many times as you want until you bust or stand. You also have the option to double and split. A double is when you have your initial hand and you double your bet and get one more card. A split is when you have two cards of the same rank and you split them into two hands and maintain your bet for both hands. 
+          <li className="text-balance p-3">
+            3.) Click Hit to be dealt another card. You can hit as many times as you want until you bust or stand. You also have the option to double and split. 
+            A double is when you have your initial hand and you double your bet and get one more card. A split is when you have two cards of the same rank and you split them into two hands and maintain your bet for both hands. 
           </li>
-          <li>
+          <li className="p-3">
             4.) Click Stand to end your turn. The dealer will then play their hand. The dealer must hit if they have a score of 16 or less and must stand if they have a score of 17 or more.
           </li>
-          <li>
+          <li className="p-3">
             5.) If you win, you will win your bet. If you lose, you will lose your bet. If you get blackjack, you will win 1.5x your bet. 
           </li>
-          <li>
+          <li className="p-3">
             6.) If you run out of chips, you lose the game.
           </li>
 
@@ -108,7 +113,7 @@ const Table = () => {
     </Modal>
   );
 
-    return (<div className="flex flex-col border-2 border-white h-screen ">
+    return (<div className="flex flex-col border-2 border-white">
         
         {showModal && modal}
         {showModal2 && modal2}
